cc5a2f052bb63c65e7105d1827ef91236e5423e0
artanger <artimag666@gmail.com>
2021-02-24 18:06:06 +0200

    initial code

---
+++ ClientBonusSystem/CardApiClient.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Configuration;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientBonusSystem
+{
+    public class CardApiClient
+    {
+        public HttpResponseMessage Get(string url)
+        {
+            try
+            {
+                var noSslHandler = new HttpClientHandler()
+                {
+                    ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
+                };
+
+                using (var client = new HttpClient(noSslHandler))
+                {
+                    var apiHostUrl = ConfigurationManager.AppSettings["ApiHostUrl"];
+
+                    client.BaseAddress = new Uri(apiHostUrl);
+                    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                    return client.GetAsync(url).Result;
+                }                   
+            }
+            catch (Exception ex)
+            {
+                return new HttpResponseMessage
+                {
+                    StatusCode = System.Net.HttpStatusCode.InternalServerError,
+                    ReasonPhrase = ex.Message
+                };
+            }
+        }
+
+        public HttpResponseMessage Post(string url, string postParams)
+        {
+            try
+            {
+                var noSslHandler = new HttpClientHandler()
+                {
+                    ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
+                };
+
+                using (var client = new HttpClient(noSslHandler))
+                {                  
+                    var apiHostUrl = ConfigurationManager.AppSettings["ApiHostUrl"];
+
+                    client.BaseAddress = new Uri(apiHostUrl);
+                    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+
+                    var response = client.PostAsync(url, new StringContent(postParams, Encoding.UTF8, "application/json"));
+                    response.Wait();
+
+
+                    return response.Result;
+
+                }
+            }
+            catch (Exception ex)
+            {
+                return new HttpResponseMessage
+                {
+                    StatusCode = System.Net.HttpStatusCode.InternalServerError,
+                    ReasonPhrase = ex.Message
+                };
+            }
+        }
+    }
+}
---
+++ ClientBonusSystem/Models/AddParamsModel.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClientBonusSystem.Models
+{
+    public class AddParamsModel
+    {
+        [JsonProperty(PropertyName = "firstName")]
+        public string FirstName { get; set; }
+
+        [JsonProperty (PropertyName ="lastName")]
+        public string LastName { get; set; }
+
+        [JsonProperty(PropertyName = "phone")]
+        public string PhoneNumber { get; set; }
+    }
+}
